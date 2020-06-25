97a8e9a00499550aeef777a8c2719cc1d069f4f6
Kelian Baert <kelian@baert.me>
2020-06-25 23:07:27 +0200

    Added output location to constants

--- src/constants.js
+++ src/constants.js
@@ -3,11 +3,12 @@
 export const URL_IMSDB = "https://www.imsdb.com/";
 export const URL_IMSDB_ALL_SCRIPTS = URL_IMSDB + "all scripts/"
 
-// Local directory paths
+// Local directory & file paths
 
 export const CACHE_DIR = "./cache/"
 export const CACHE_SCRIPTS_DIR = CACHE_DIR + "raw-scripts/";
 export const CACHE_METADATA_FILE = CACHE_DIR + "metadata.json";
+export const OUTPUT_DIR = "./output/";
 
 // Movie annotation types
 
