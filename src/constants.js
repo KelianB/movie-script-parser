fec34ad039f44c602338b9133e04be8157fb6223
Kelian Baert <kelian@baert.me>
2020-06-28 21:04:12 +0200

    Added constants for the size of fetch batches

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,13 @@ export const CACHE_SCRIPTS_DIR = CACHE_DIR + "raw-scripts/";
 export const CACHE_METADATA_FILE = CACHE_DIR + "metadata.json";
 export const OUTPUT_DIR = "./output/";
 
+// Parameters for fetching data
+
+// Number of parallel requests
+export const BATCH_SIZE_FETCH_SCRIPTS = 5;
+export const BATCH_SIZE_FETCH_METADATA = 5;
+
+
 // Movie annotation types
 
 export const Annotation = {
